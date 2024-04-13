b73440dc8607721d298a80cab3b9dea395d35b86
tcrogliO <tiago.roglio2112@gmail.com>
2024-04-12 22:06:59 -0300

    ajustando o código

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-function darkMode() {
+function switchMode() {
 
-    /* dark mode all */
-    document.documentElement.style.setProperty('--background-primary-color', 'rgb(65, 63, 63)');  /* cinza escuro background */
-    document.documentElement.style.setProperty('--font-primary-color', 'white'); /* branco do texto */
-    document.documentElement.style.setProperty('--font-secundary-color', 'gold'); /* dourado dos títulos */
+    var backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--background-primary-color');
 
-    /* dark mode header and footer */
-    document.documentElement.style.setProperty('--background-both-primary-color', '#232222'); /* cinza escuro do header e do footer de baixo */
-    document.documentElement.style.setProperty('--background-both-secundary-color', 'rgb(50, 50, 50)'); /* cinza mais claro do footer de cima */
-    document.documentElement.style.setProperty('--font-both-primary-color', 'white'); /* branco do texto */
-}
+    if (backgroundColor == 'white') { /* TRANSFORMA EM PRETO */
+        /* dark mode all */
+        document.documentElement.style.setProperty('--background-primary-color', 'rgb(65, 63, 63)');  /* cinza escuro background */
+        document.documentElement.style.setProperty('--font-primary-color', 'white'); /* branco do texto */
+        document.documentElement.style.setProperty('--font-secundary-color', 'gold'); /* dourado dos títulos */
 
-    
-function lightMode() {
+        /* dark mode header and footer */
+        document.documentElement.style.setProperty('--background-both-primary-color', '#232222'); /* cinza escuro do header e do footer de baixo */
+        document.documentElement.style.setProperty('--background-both-secundary-color', 'rgb(50, 50, 50)'); /* cinza mais claro do footer de cima */
+        document.documentElement.style.setProperty('--font-both-primary-color', 'white'); /* branco do texto */
 
-    /* white mode all */
-    document.documentElement.style.setProperty('--background-primary-color', 'white');
-    document.documentElement.style.setProperty('--font-primary-color', 'black');
-    document.documentElement.style.setProperty('--font-secundary-color', 'black');
+        let p = document.getElementById('darkorlight');
+        p.innerHTML = "LIGHT MODE"
 
-    /* white mode all */
-    document.documentElement.style.setProperty('--background-both-primary-color', 'grey');
-    document.documentElement.style.setProperty('--background-both-secundary-color', 'lightgrey');
-    document.documentElement.style.setProperty('--font-both-primary-color', 'black');
+    } else { /* TRANSFORMA EM PRETO */
+
+        /* white mode all */
+        document.documentElement.style.setProperty('--background-primary-color', 'white');
+        document.documentElement.style.setProperty('--font-primary-color', 'black');
+        document.documentElement.style.setProperty('--font-secundary-color', 'black');
+
+        /* white mode all */
+        document.documentElement.style.setProperty('--background-both-primary-color', 'grey');
+        document.documentElement.style.setProperty('--background-both-secundary-color', 'lightgrey');
+        document.documentElement.style.setProperty('--font-both-primary-color', 'black');
+        let p = document.getElementById('darkorlight');
+        p.innerHTML = "DARK MODE"
+    }
 }
